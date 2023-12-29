dd3c86bd710d9a96f66f551469d6ba6c824cde77
Victor Maldonado <victoramaldonadog@gmail.com>
2023-12-29 03:18:56 -0400

    FIX:identity details and logout

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ROOT_URLCONF = 'core.urls'
 LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
-LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
+LOGOUT_REDIRECT_URL = "login"  # Route defined in home/urls.py
 TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
 
 TEMPLATES = [
